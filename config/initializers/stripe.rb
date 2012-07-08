6ae4f03e5c41b53c05517479732b8a03d187c7ff
Joel Johnson <johnson.joel.b@gmail.com>
2012-07-08 08:07:40 -0500

    Can't log in initializers apparently

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,6 +3,5 @@ if Rails.env.development? or Rails.env.test?
     require File.expand_path('../test_stripe', __FILE__)
 end
 
-logger.debug "Initializing stripe..."
 Stripe.api_key = ENV["STRIPE_PRIVATE_KEY"]
 STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"]
