09d3d70882776157141fa53f7d833edbec080691
Joel Johnson <johnson.joel.b@gmail.com>
2012-07-08 12:34:36 -0500

    Update news feed to use correct tumblr, and restyled news items. Updated other wells to match the style.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,7 +4,7 @@ require 'tumblr4r'
 class NewsController < ApplicationController
     def index
         @tweets = Twitter.user_timeline("ArchaicSmiles")
-        @tumbles = Tumblr4r::Site.new("archaic-smiles.tumblr.com").find(:all, :limit => 50)
+        @tumbles = Tumblr4r::Site.new("artinterrupted.tumblr.com").find(:all, :limit => 50)
     end
 
     def caller
