ea7914ce3eaf494f5a54eb2f8234031b09eb83a2
Joel Johnson <johnson.joel.b@gmail.com>
2012-07-09 19:13:29 -0500

    .

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -5,7 +5,7 @@ end
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.com',
     :port => '587',
-    :authentication => :plain
+    :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => ENV['SENDGRID_DOMAIN']
