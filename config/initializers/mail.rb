e91ee4e0fb12cf69a1b1ea782db9f8b954448d90
Joel Johnson <johnson.joel.b@gmail.com>
2012-07-10 23:03:56 -0500

    .

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -9,6 +9,7 @@ ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => ENV['SENDGRID_DOMAIN']
+    :enable_starttls_auto => true
 }
 
 ActionMailer::Base.raise_delivery_errors = true
