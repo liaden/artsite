2dcf27b5d84a46bdba25a325dbee21be0ed4f9fa
Joel Johnson <johnson.joel.b@gmail.com>
2012-07-13 20:44:28 -0500

    Change to new tumblr.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -7,7 +7,7 @@ class NewsController < ApplicationController
 
     def index
         @tweets = Twitter.user_timeline("ArchaicSmiles")
-        @tumbles = Tumblr4r::Site.new("artinterrupted.tumblr.com").find(:all, :limit => 50)
+        @tumbles = Tumblr4r::Site.new("archaic-smiles.tumblr.com").find(:all, :limit => 50)
         render :index
     end
 
