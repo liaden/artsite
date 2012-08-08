1d814737b8c6e959edf67b67c41feaeea97302ae
Joel Johnson <johnson.joel.b@gmail.com>
2012-08-07 19:16:10 -0500

    Handle twitter sucking cock.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -6,9 +6,17 @@ class NewsController < ApplicationController
     caches_action :index, :expires_in => 1.hours
 
     def index
-        @tweets = Twitter.user_timeline("ArchaicSmiles")
+        begin 
+            @tweets = Twitter.user_timeline("ArchaicSmiles") 
+        rescue Twitter::Error::BadRequest => e
+            @tweets = []
+        end
+
         @tumbles = Tumblr4r::Site.new("archaic-smiles.tumblr.com").find(:all, :limit => 50)
         render :index
+    rescue Twitter::Error::BadRequest => e
+        @tweets = []
+        
     end
 
     def caller
