1eee2877e48d072871d111e685950afed0bfec17
Joel Johnson <johnson.joel.b@gmail.com>
2014-01-01 12:56:03 -0600

    Get news feed working by fixing twitter interop to use embedded timeline

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,3 @@
-require 'twitter'
 require 'tumblr4r'
 
 class NewsController < ApplicationController
@@ -6,12 +5,6 @@ class NewsController < ApplicationController
     caches_action :index, :expires_in => 1.hours
 
     def index
-        begin 
-            @tweets = Twitter.user_timeline("ArchaicSmiles") 
-        rescue Twitter::Error::BadRequest => e
-            @tweets = []
-        end
-
         @tumbles = Tumblr4r::Site.new("archaic-smiles.tumblr.com").find(:all, :limit => 50)
         render :index
     end
