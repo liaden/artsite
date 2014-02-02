593f733fdeaca196fbc59f2addd1ae852f1ef0b6
Joel Johnson <johnson.joel.b@gmail.com>
2014-02-02 12:45:26 -0600

    Don't share token on github.
    Regenerated token so new one is used.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ArchaicSmiles::Application.config.secret_token = 'f9535ebd5b9d6d153cfbe03e07af422b233e3eae03dd6b8a5cd31f57386615e4257f6bc25736fb09b8ed2f1d7d122cb1efbd7a44429eded7389c2fe7eada9934'
