623b5881e0c4f57aefda6b2e7d7027a14204f56f
Joel Johnson <johnson.joel.b@gmail.com>
2014-03-12 21:35:41 -0500

    Convert news page layout to foundation.
    Instead of embedding the entire twitter news feed, pull the oembed
    version of the tweets and display them that way.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,8 +5,12 @@ class NewsController < ApplicationController
     caches_action :index, :expires_in => 1.hours
 
     def index
-        @tumbles = Tumblr4r::Site.new("archaic-smiles.tumblr.com").find(:all, :limit => 50)
-        render :index
+
+      @tweets = Tweet.last_seven_days
+      puts @tweets.count
+
+      @tumbles = Tumblr4r::Site.new("archaic-smiles.tumblr.com").find(:all, :limit => 50)
+      render :index
     end
 
     def caller
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,5 @@ class Tweet < ActiveRecord::Base
   validates_uniqueness_of :twitter_id
   validates :html, :twitter_id, :presence => true
 
+  scope :last_seven_days, where('created_at > ?', 7.days.ago)
 end
