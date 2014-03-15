ec539bc64a6179cfbe76ea22b7e9a6592d77bbf2
Joel Johnson <johnson.joel.b@gmail.com>
2014-03-15 18:12:47 -0500

    Cleanup broken tests from foundation changes.

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -2,128 +2,127 @@ require 'twitter'
 require 'tumblr4r'
 
 class ArtworksController < ApplicationController
-    decorates_assigned :artwork
+  decorates_assigned :artwork
 
-    before_filter :require_admin, :only => [:new, :create, :edit, :update, :destroy ]
-    before_filter :set_artwork, :only  => [:edit, :update, :show, :destroy ]
+  before_filter :require_admin, :only => [:new, :create, :edit, :update, :destroy ]
+  before_filter :set_artwork, :only  => [:edit, :update, :show, :destroy ]
 
-    respond_to :html, :json
+  respond_to :html, :json
 
-    def index
-      @category = 'Featured'
-      @artworks = Artwork.featured( :order => "created_at DESC")
-    end
+  def index
+    @category = 'Featured'
+    @artworks = Artwork.featured( :order => "created_at DESC") || []
+  end
 
-    def new
-      @artwork = Artwork.new
-      @tags = ""
-      @mediums = ""
+  def new
+    @artwork = Artwork.new
+    @tags = ""
+    @mediums = ""
 
-      # defaults sizes for a 2:3 ratio
-      @sizes = [ '4x6', '8x12', '12x18' ]
-    end
+    # defaults sizes for a 2:3 ratio
+    @sizes = [ '4x6', '8x12', '12x18' ]
+  end
 
-    def create
-      ActiveRecord::Base.transaction do
-        @artwork = Artwork.new params[:artwork] do |art|
-          art.create_tags_from_csv params[:tags]
-          art.create_medium_from_csv params[:mediums]
-        end
-        @artwork.created_at = Date.strptime(params[:artwork][:created_at], '%m/%d/%Y') 
-
-        if @artwork.save
-          flash[:notice] = "'#{@artwork.title}' has been successfully created."
-          return redirect_to(artwork_prints_path(@artwork))
-        else
-          flash.now[:error] = "Error saving artwork"
-          logger.debug @artwork.errors.messages
-
-          return render :action => :new
-        end
+  def create
+    ActiveRecord::Base.transaction do
+      @artwork = Artwork.new params[:artwork] do |art|
+        art.create_tags_from_csv params[:tags]
+        art.create_medium_from_csv params[:mediums]
       end
+      @artwork.created_at = Date.strptime(params[:artwork][:created_at], '%m/%d/%Y') if params[:artwork][:created_at]
 
-      render :action => :new
-    end
-
-    def show
-      if request.path != artwork_path(@artwork)
-        redirect_to @artwork, :status => :moved_permanently
-      end
-
-      impressionist(@artwork)
-    end
-
-    def edit
-      @tags = @artwork.tags_csv
-      @medium = @artwork.medium_csv
-    end
-
-    def update
-       ActiveRecord::Base.transaction do
-         
-         @artwork.create_tags_from_csv params[:tags] if params[:tags]
-         @artwork.create_medium_from_csv params[:medium] if params[:medium]
-
-         respond_to do |format|
-           if @artwork.update_attributes(params[:artwork])
-             format.html { redirect_to(@artwork, :notice => "Artwork #{@artwork.title} has been successfully updated.") }
-             format.json do
-               if params[:bip] == 'skip'
-                 respond_with @artwork.to_json
-              else
-                respond_with_bip(@artwork) 
-              end
-             end
-           else
-             format.html { render :action => "edit" }
-             format.json { respond_with_bip(@artwork) }
-           end
-         end
-      end
-    
-    end
-
-    def filter_by_category
-      categories = %w(All Featured Fanart)
-      @category = params[:category] || 'Featured'
-
-      if @category.match /^\d\d\d\d$/
-        @artworks = Artwork.for_year(@category)
-      elsif categories.any? { |category| category == @category  }
-        @artworks = Artwork.send(@category.downcase, :order => 'created_at DESC') 
+      if @artwork.save
+        flash[:notice] = "'#{@artwork.title}' has been successfully created."
+        return redirect_to(artwork_prints_path(@artwork))
       else
-        tag = Tag.find_by_name(@category)
-        @artworks = tag.artworks if tag
+        flash.now[:error] = "Error saving artwork"
+        logger.debug @artwork.errors.messages
+
+        return render :action => :new
       end
-
-      @artworks ||= []
-
-      render :index
     end
 
-    def destroy
-      return redirect_to :action => :index unless admin?
+    render :action => :new
+  end
 
-      Artwork.transaction do
-        @artwork.destroy
-      end
+  def show
+    if request.path != artwork_path(@artwork)
+      redirect_to @artwork, :status => :moved_permanently
+    end
 
-      @artworks = Artwork.all
+    impressionist(@artwork)
+  end
+
+  def edit
+    @tags = @artwork.tags_csv
+    @medium = @artwork.medium_csv
+  end
+
+  def update
+    ActiveRecord::Base.transaction do
+      
+      @artwork.create_tags_from_csv params[:tags] if params[:tags]
+      @artwork.create_medium_from_csv params[:medium] if params[:medium]
 
       respond_to do |format|
-        format.html { redirect_to artworks_path }
-        format.json { respond_with {} }
+        if @artwork.update_attributes(params[:artwork])
+          format.html { redirect_to(@artwork, :notice => "Artwork #{@artwork.title} has been successfully updated.") }
+          format.json do
+            if params[:bip] == 'skip'
+              respond_with @artwork.to_json
+           else
+             respond_with_bip(@artwork) 
+           end
+          end
+        else
+          format.html { render :action => "edit" }
+          format.json { respond_with_bip(@artwork) }
+        end
       end
     end
+  end
 
-    def caller
-      "Gallery"
+  def filter_by_category
+    categories = %w(All Featured Fanart)
+    @category = params[:category] || 'Featured'
+
+    if @category.match /^\d\d\d\d$/
+      @artworks = Artwork.for_year(@category)
+    elsif categories.any? { |category| category == @category  }
+      @artworks = Artwork.send(@category.downcase, :order => 'created_at DESC') 
+    else
+      tag = Tag.find_by_name(@category)
+      @artworks = tag.artworks if tag
     end
+
+    @artworks ||= []
+
+    render :index
+  end
+
+  def destroy
+    return redirect_to :action => :index unless admin?
+
+    Artwork.transaction do
+      @artwork.destroy
+    end
+
+    @artworks = Artwork.all
+
+    respond_to do |format|
+      format.html { redirect_to artworks_path }
+      format.json { respond_with {} }
+    end
+  end
+
+  def caller
+    "Gallery"
+  end
 
 private
-    def artworks
-      @decorated_artworks ||= ArtworksDecorator.decorate(@artworks, :context => { :category => @category } )
-    end
+  def artworks
+    @decorated_artworks ||= ArtworksDecorator.decorate(@artworks, :context => { :category => @category } )
+  end
 
-    helper_method :artworks
+  helper_method :artworks
 end
--- spec/controllers/artworks_controller_spec.rb
+++ spec/controllers/artworks_controller_spec.rb
@@ -1,200 +1,198 @@
 require 'spec_helper'
 
 describe ArtworksController do
-    let(:artwork) { FactoryGirl.create(:artwork) }
-    let(:tagged_artwork) { FactoryGirl.create(:tagged_artwork) }
-    let(:artwork_in_medium) { FactoryGirl.create(:artwork_in_medium) }
+  let(:artwork) { FactoryGirl.create(:artwork) }
+  let(:tagged_artwork) { FactoryGirl.create(:tagged_artwork) }
+  let(:artwork_in_medium) { FactoryGirl.create(:artwork_in_medium) }
 
-    before(:each) { mock_paperclip_post_process }
+  before(:each) { mock_paperclip_post_process }
 
-    before(:each) do
+  before(:each) do
 
-        @containing_folder = './spec/images/'
-        @watercolor = "#{@containing_folder}watercolor.png"
-        @create = "#{@containing_folder}create.jpg"
-        @rails = "#{@containing_folder}rails.png"
+    @containing_folder = './spec/images/'
+    @watercolor = "#{@containing_folder}watercolor.png"
+    @create = "#{@containing_folder}create.jpg"
+    @rails = "#{@containing_folder}rails.png"
 
-        DefaultPrice.add_test_defaults_to_database
+    DefaultPrice.add_test_defaults_to_database
+  end
+
+  context "as a guest" do
+    let(:item) { artwork }
+    let(:redirect_url) { artworks_path }
+    let(:attrs) { FactoryGirl.attributes_for(:artwork) }
+    let(:table) { Artwork }
+
+    it_behaves_like 'unauthorized GET new'
+    it_behaves_like 'unauthorized POST create'
+    it_behaves_like 'unauthorized GET edit'
+    it_behaves_like 'unauthorized DELETE destroy'
+
+    describe "GET index" do
+      it "shows featured artworks" do
+        artwork.update_attributes :featured => true
+
+        get :index
+        assigns[:artworks].should_not be_empty
+      end
+
+      it "does not show unfeatured artworks" do
+        artwork.update_attributes :featured => false
+
+        get :index
+        assigns[:artworks].should be_empty
+      end
     end
 
-    context "as a guest" do
-        let(:item) { artwork }
-        let(:redirect_url) { artworks_path }
-        let(:attrs) { FactoryGirl.attributes_for(:artwork) }
-        let(:table) { Artwork }
+    describe "GET fillter" do
+      context 'renders views' do
+        render_views
 
-        it_behaves_like 'unauthorized GET new'
-        it_behaves_like 'unauthorized POST create'
-        it_behaves_like 'unauthorized GET edit'
-        it_behaves_like 'unauthorized DELETE destroy'
+        it "renders views without errors with no artworks" do
+          expect { 
+            get :filter_by_category, :category => '2004'
+          }.to_not raise_error
 
-        describe "GET index" do
-            it "assigns @artworks" do
-                artwork
+          assigns[:artworks].should be_empty
+        end
+      end
 
-                get :index
-                assigns[:artworks].should_not be_empty
-            end
+      context 'finds by' do
+        after(:each) { assigns[:artworks].should have(1).item }
+      
+        it "year" do
+          FactoryGirl.create(:artwork, :created_at => DateTime.new(2012, 1, 5))
+          get :filter_by_category, :category => '2012'
         end
 
-        #describe "GET new" do
-        #    it "redirects" do
-        #        get :new
-        #        response.should redirect_to(artworks_path)
-        #    end
-        #end
+        it "tag" do
+          FactoryGirl.create(:tagged_artwork)
+          get :filter_by_category, :category => Tag.first.name
+        end
 
-        #describe "POST create" do
-        #    it "redirects" do
-        #        expect { post :create
-        #               }.to change { Artwork.count }.by(0)
+        it "fanart" do
+          FactoryGirl.create(:artwork, :fanart => true) 
+          get :filter_by_category, :category => 'Fanart'
+        end
+      end
+    end
+  end
 
-        #        response.should redirect_to(artworks_path) 
-        #    end
+  context "as an admin" do
+    before(:each) { login :admin }
 
-        #    it "does not create a new artwork" do
-        #        expect { post :create }.to change { Artwork.count }.by(0)
-        #   end
-
-        #end
-
-        #describe "GET edit" do
-        #    it "redirects" do
-        #        get :edit, :id => artwork.id
-        #        response.should redirect_to(artworks_path)
-        #    end
-        #end
-
-        #describe "PUT update" do
-        #    it "redirects" do
-        #        put :update, :id => artwork.id, :artwork => { :title => 'DIFFERENT' }
-        #        response.should redirect_to(artworks_path)
-        #    end
-
-        #    it "does not update teh artwork" do
-        #        put :update, :id => artwork.id, :artwork => { :title => 'DIFFERENT' }
-        #        artwork.reload.title.should_not eq('DIFFERENT')
-        #    end
-        #end
+    describe "GET new" do
+      it "assigns artwork" do
+        get :new
+        assigns(:artwork).should be_an_instance_of(Artwork)
+      end
     end
 
-    context "as an admin" do
-        before(:each) { login :admin }
+    describe "POST create" do
+      before(:each) do
+          @attrs = { :artwork => FactoryGirl.attributes_for(:artwork, :image => Rack::Test::UploadedFile.new(@rails)),
+                     :tags => "big,buterflies,dancing",
+                     :mediums => "watercolor",
+                   }
+      end
 
-        describe "GET new" do
-            it "assigns artwork" do
-                get :new
-                assigns(:artwork).should be_an_instance_of(Artwork)
-            end
-        end
+      it "saves a new artwork" do
+          expect {
+              post :create, @attrs
+          }.to change {Artwork.count }.by(1)
+      end
 
-        describe "POST create" do
-            before(:each) do
-                @attrs = { :artwork => FactoryGirl.attributes_for(:artwork, :image => Rack::Test::UploadedFile.new(@rails)),
-                           :tags => "big,buterflies,dancing",
-                           :mediums => "watercolor",
-                         }
-            end
+      #it "saves with only the original prints" do
+      #    expect {
+      #        post :create, @attrs
+      #    }.to change { Print.count }.by(1)
+      #end
 
-            it "saves a new artwork" do
-                expect {
-                    post :create, @attrs
-                }.to change {Artwork.count }.by(1)
-            end
+      #it "saves only one in the small size" do
+      #    @attrs[:enable_s] = "yes"
 
-            #it "saves with only the original prints" do
-            #    expect {
-            #        post :create, @attrs
-            #    }.to change { Print.count }.by(1)
-            #end
+      #    expect {
+      #        post :create, @attrs
+      #    }.to change { Print.count }.by(2)
+      #end
 
-            #it "saves only one in the small size" do
-            #    @attrs[:enable_s] = "yes"
+      #it "saves 8 prints with all options selected"  do
+      #    expect {
+      #        post :create, @attrs
+      #    }.to change { Print.count }.by(8)
+      #
+      #end
 
-            #    expect {
-            #        post :create, @attrs
-            #    }.to change { Print.count }.by(2)
-            #end
+      #it "saves with the original already sold out"  do
+      #    @attrs[:is_sold_out] = "yes"
+      #
+      #    post :create, @attrs
+      #    artwork = Artwork.last
+      #
+      #    artwork.original.is_sold_out.should == true
+      #end
 
-            #it "saves 8 prints with all options selected"  do
-            #    expect {
-            #        post :create, @attrs
-            #    }.to change { Print.count }.by(8)
-            #
-            #end
+      #it "does not create an artwork given a bad size" do
+      #    @attrs[:original_size] = "not a valid size"
 
-            #it "saves with the original already sold out"  do
-            #    @attrs[:is_sold_out] = "yes"
-            #
-            #    post :create, @attrs
-            #    artwork = Artwork.last
-            #
-            #    artwork.original.is_sold_out.should == true
-            #end
+      #    expect {
+      #        post :create, @attrs
+      #    }.to_not change { Artwork.count }
+      #end
 
-            #it "does not create an artwork given a bad size" do
-            #    @attrs[:original_size] = "not a valid size"
-
-            #    expect {
-            #        post :create, @attrs
-            #    }.to_not change { Artwork.count }
-            #end
-
-            #it "does not create any prints given a bad size" do
-            #    expect { 
-            #        @attrs[:original_size] = "not a valid size"
-            #        post :create, @attrs
-            #    }.to_not change { Artwork.count }
-            #end
-
-
-        end
-
-        describe "GET edit" do
-
-            it "sets the artwork" do
-                get :edit, :id => artwork.id
-                assigns[:artwork].should == artwork
-            end
-
-            it "sets csv tags" do
-                get :edit, :id => tagged_artwork.id
-                assigns[:tags].should_not be_empty
-            end
-            it "sets csv mediums" do
-                get :edit, :id => artwork_in_medium.id
-                assigns[:medium].should_not be_empty
-            end
-
-        end
-
-        describe "PUT update" do
-            it "updates the attributes" do
-                post :update, :id => artwork.id, :artwork => { :title => 'DIFFERENT' }
-                artwork.reload.title.should == 'DIFFERENT'
-            end
-
-            it "updates new tags" do
-                post :update, :id => artwork.id, :artwork => {}, :tags => 'x,y,z'
-                sorted_tag_names = artwork.tags.map(&:name).sort 
-                
-                sorted_tag_names.should == ['x', 'y', 'z']
-            end
-
-        end
-
-        describe "POST destroy" do
-
-            it "destroys the artwork" do
-                artwork
-                expect { post :destroy, :id => artwork.id }.to change{Artwork.count}.by(-1)
-            end
-
-            it "destroys associated prints" do
-                artwork
-                expect { post :destroy, :id => artwork.id }.to change{Print.count}.by(-1*artwork.prints.count)
-            end
-        end
+      #it "does not create any prints given a bad size" do
+      #    expect { 
+      #        @attrs[:original_size] = "not a valid size"
+      #        post :create, @attrs
+      #    }.to_not change { Artwork.count }
+      #end
     end
 
+    describe "GET edit" do
+
+      it "sets the artwork" do
+        get :edit, :id => artwork.id
+        assigns[:artwork].should == artwork
+      end
+
+      it "sets csv tags" do
+        get :edit, :id => tagged_artwork.id
+        assigns[:tags].should_not be_empty
+      end
+
+      it "sets csv mediums" do
+        get :edit, :id => artwork_in_medium.id
+        assigns[:medium].should_not be_empty
+      end
+
+    end
+
+    describe "PUT update" do
+      it "updates the attributes" do
+        post :update, :id => artwork.id, :artwork => { :title => 'DIFFERENT' }
+        artwork.reload.title.should == 'DIFFERENT'
+      end
+
+      it "updates new tags" do
+        post :update, :id => artwork.id, :artwork => {}, :tags => 'x,y,z'
+        sorted_tag_names = artwork.tags.map(&:name).sort 
+          
+        sorted_tag_names.should == ['x', 'y', 'z']
+      end
+
+    end
+
+    describe "POST destroy" do
+
+      it "destroys the artwork" do
+        artwork
+        expect { post :destroy, :id => artwork.id }.to change{Artwork.count}.by(-1)
+      end
+
+      it "destroys associated prints" do
+        artwork
+        expect { post :destroy, :id => artwork.id }.to change{Print.count}.by(-1*artwork.prints.count)
+      end
+    end
+  end
 end
