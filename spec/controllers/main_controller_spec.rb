c4943d2c9f6065a7b249082343501d3d8b109ba4
Joel Johnson <johnson.joel.b@gmail.com>
2014-03-15 18:13:42 -0500

    Improved test coverage.

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,31 +1,60 @@
 require 'spec_helper'
 
 describe MainController do
-    before(:each) { mock_paperclip_post_process }
+  before(:each) { mock_paperclip_post_process }
+  before(:each) { FactoryGirl.create(:artwork) }
 
-    describe "GET index" do
-        it "should assign @artworks" do
-            FactoryGirl.create(:artwork)
+  describe "GET index" do
+    it "should assign @artwork" do
+      FactoryGirl.create(:artwork)
 
-            get :index
-            assigns(:artworks).first.should be_an_instance_of(Artwork)
-        end
-
-        it "should have empty list with no art" do
-            get :index
-            assigns(:artworks).should be_empty
-        end
-
-        it "should get at most five artworks" do
-            1.upto(10) do 
-                FactoryGirl.create(:artwork)
-            end
-
-            get :index
-
-            assigns(:artworks).size.should eq(MainController::MOST_RECENT_LIMIT)
-        end
-
+      get :index
+      assigns(:artwork).should be_an_instance_of(Artwork)
     end
+
+    it "should assign the next show" do
+      FactoryGirl.create(:show, :date => 7.days.from_now)
+
+      get :index
+      assigns(:next_show).should be_an_instance_of(Show)
+    end
+
+    context 'when it renders view' do
+      render_views
+
+      it 'no upcoming shows does not raise an error' do
+        expect { get :index }.to_not raise_error
+      end
+    end
+  end
+
+  describe 'subnavbar' do
+    render_views
+
+    context 'as a guest' do
+      it 'returns nothing' do
+        get :subnavbar
+        response.body.should be_empty
+      end
+    end
+
+    context 'as a user' do
+      it 'returns nothing' do
+        login :user
+
+        get :subnavbar
+        response.body.should be_empty
+      end
+    end
+
+    context 'as admin' do
+      it 'returns the subnav' do
+        login :admin
+
+        get :subnavbar
+        response.should render_template('admin_sub_navbar')
+      end
+    end
+  end
 end
 
