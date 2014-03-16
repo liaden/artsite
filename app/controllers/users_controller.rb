cfd0753bcbf894fa0257d4d52676a703c99bcf70
Joel Johnson <johnson.joel.b@gmail.com>
2014-03-15 19:22:22 -0500

    Fix typo in notice.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 class UsersController < ApplicationController
-    decorates_assigned :user
+  decorates_assigned :user
 
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new params[:user]
+
+    # force everyone to be regular users
+    @user.privilege = 0
+
+    if @user.save
+      redirect_to home_path, :notice => "Account successfully created."
+    else
+      render :action => :new
     end
+  end
 
-    def create
-        @user = User.new params[:user]
+  def history
+    @user = current_user
+    @order = @user.old_orders.sorty_by { |order| order.placed_at }
 
-        # force everyone to be regular users
-        @user.privilege = 0
-
-        if @user.save
-            redirect_to :controller => :main, :action => :index, :notice => "Account succusfully created."
-        else
-            render :action => :new
-        end
+    if not @user
+      flash[:error] = "Please login to view this information."
     end
-
-    def history
-        @user = current_user
-        @order = @user.old_orders.sorty_by { |order| order.placed_at }
-
-        if not @user
-            flash[:error] = "Please login to view this information."
-        end
-    end
+  end
 end
