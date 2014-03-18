766900bcc37f9e77bc3f88746811918cd6a338ea
Joel Johnson <johnson.joel.b@gmail.com>
2014-03-17 20:53:58 -0500

    Cleanup social media urls

---
+++ app/helpers/social_media_helper.rb
@@ -0,0 +1,17 @@
+module SocialMediaHelper
+    def twitter_url
+      'http://twitter.com/archaicsmiles'
+    end
+
+    def tumblr_url
+     'http://archaicsmiles.tumblr.com' 
+    end
+
+    def facebook_url
+      'https://www.facebook.com/pages/Archaicsmiles/1436457433240554'
+    end
+
+    def pinterest_url
+      'http://www.pinterest.com/archaicsmiles/'
+    end
+end
