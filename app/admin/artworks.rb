8588e7da9540195827eea35f3ebbb10fb53e1dfa
Joel Johnson <johnson.joel.b@gmail.com>
2014-05-02 13:53:24 -0500

    Configuring admin pages for artworks and prints.

--- app/admin/artworks.rb
+++ app/admin/artworks.rb
@@ -1,2 +1,13 @@
 ActiveAdmin.register Artwork do
+  decorate_with ArtworkDecorator
+
+  index do
+    column(:image) do |artwork|
+      image_tag(artwork.image.url(:thumbnail), :class => 'mini-thumbnail')
+    end
+    column(:title)
+    column(:featured)
+    column(:fanart)
+    column(:created) 
+  end
 end
--- app/admin/prints.rb
+++ app/admin/prints.rb
@@ -1,2 +1,15 @@
 ActiveAdmin.register Print do
+  decorate_with PrintDecorator
+
+  index do
+    column(:artwork) do |print|
+      image_tag(print.artwork_thumbnail_url, :class => 'mini-thumbnail')
+    end
+    column(:price)
+    column(:is_sold_out)
+    column(:is_on_show)
+    column(:material)
+    column(:inventory_count)
+    column(:sold_count)
+  end
 end
