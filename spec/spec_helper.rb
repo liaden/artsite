4acd1d0e72f721dc3130c64a652fd3dec9490619
Joel Johnson <johnson.joel.b@gmail.com>
2015-01-03 11:10:41 -0600

    Move prints controller over to strong param

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,6 @@ require 'database_cleaner'
 require 'authlogic/test_case'
 require 'best_in_place/test_helpers'
 
-binding.pry 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
