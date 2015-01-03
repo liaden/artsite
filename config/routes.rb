2cc3956a8f5bd89e4f79d36e1f994f96dcf9f419
Joel Johnson <johnson.joel.b@gmail.com>
2015-01-03 10:14:08 -0600

    Remove usage of match in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,22 @@ ArchaicSmiles::Application.routes.draw do
   root :to => 'main#index'
   ActiveAdmin.routes(self)
 
-  match '/artworks/filter/:category/' => 'artworks#filter_by_category', :as => :artworks_by_category
+  get '/artworks/filter/:category/' => 'artworks#filter_by_category', :as => :artworks_by_category
 
   resources :artworks do
-      resources :prints do
-          collection do
-              get 'canvas'
-              get 'photopaper'
-              get 'original'
-          end
+    resources :prints do
+      collection do
+        get 'canvas'
+        get 'photopaper'
+        get 'original'
       end
+    end
   end
 
   resources :tags do
-      collection do
-          post 'clear_orphans'
-      end
+    collection do
+      post 'clear_orphans'
+    end
   end
 
   resources :lessons, :path => 'classes/'
@@ -32,29 +32,29 @@ ArchaicSmiles::Application.routes.draw do
   resources :supplies
 
 
-  match '/news' => 'news#index', :as => :news
-  match '/schedule' => 'schedule#index', :as => :schedule
+  get '/news' => 'news#index', :as => :news
+  get '/schedule' => 'schedule#index', :as => :schedule
   
-  match '/index' => 'main#index', :as => :home
+  get '/index' => 'main#index', :as => :home
 
-  match '/prices/edit' => 'default_price#edit', :as => :price_edit
-  match '/prices/update' => 'default_price#update', :as => :price_update
+  get '/prices/edit' => 'default_price#edit', :as => :price_edit
+  get '/prices/update' => 'default_price#update', :as => :price_update
 
-  match '/cart/checkout' => 'cart#verify_payment', :via => :post, :as => :verify_payment
-  match '/cart/checkout' => 'cart#checkout', :via => :get, :as => :checkout
-  match '/cart/purchase' => 'cart#purchase', :as => :purchase
-  match '/cart/remove/:transaction_type/:id' => 'cart#remove'
-  match '/cart' => 'cart#index', :as => :cart
+  post '/cart/checkout' => 'cart#verify_payment', :as => :verify_payment
+  get  '/cart/checkout' => 'cart#checkout', :as => :checkout
+  get  '/cart/purchase' => 'cart#purchase', :as => :purchase
+  get  '/cart/remove/:transaction_type/:id' => 'cart#remove'
+  get  '/cart' => 'cart#index', :as => :cart
 
-  match '/purchase_history' => 'users#history', :as => :purchase_history
+  get '/purchase_history' => 'users#history', :as => :purchase_history
 
-  match 'login' => 'user_sessions#new', :as => :login
-  match 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'login' => 'user_sessions#new', :as => :login
+  get 'logout' => 'user_sessions#destroy', :as => :logout
 
-  match 'inventory' => 'inventory#index', :as => :inventory
-  match 'inventory/update/:id' => 'inventory#update', :as => :inventory_update
-  match 'inventory/edit/:id' => 'inventory#edit', :as => :inventory_edit
+  get 'inventory' => 'inventory#index', :as => :inventory
+  get 'inventory/update/:id' => 'inventory#update', :as => :inventory_update
+  get 'inventory/edit/:id' => 'inventory#edit', :as => :inventory_edit
 
-  match 'main/admin_controls' => 'main#admin_controls', :as => :admin_controls
-  match 'markdown/preview' => 'main#preview_markdown', :as  => :preview_markdown
+  get 'main/admin_controls' => 'main#admin_controls', :as => :admin_controls
+  post 'markdown/preview' => 'main#preview_markdown', :as  => :preview_markdown
 end
