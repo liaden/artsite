6fbfdd2671810c3164a128995885f75cfe647b9b
Joel Johnson <joel@lendinghome.com>
2015-10-12 13:31:58 -0500

    upgrade to rails 4.1

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby "2.2.3"
 
-gem 'rails', '~> 4.0.0'
+gem 'rails', '~> 4.1.0'
 gem 'authlogic', '~> 3.4.0'
 
 gem 'activeadmin', '~> 1.0.0.pre1'
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,107 +1,112 @@
 class Artwork < ActiveRecord::Base
-   extend Taggable
+  extend Taggable
 
-   taggable Medium, :plural => :medium # JOEL TODO: fix the plural
-   taggable Tag
+  taggable Medium, :plural => :medium # JOEL TODO: fix the plural
+  taggable Tag
 
-   validates :title, :description, :image_file_name, :presence => true
-   validates :fanart, :featured, :inclusion => { :in => [true, false] }
+  validates :title, :description, :image_file_name, :presence => true
+  validates :fanart, :featured, :inclusion => { :in => [true, false] }
 
-   has_attached_file :image, {
-                       :styles => { :thumbnail => "100x100#", :medium => '250x250#' },
-                     }.merge(PAPERCLIP_STORAGE_OPTIONS)
+  has_attached_file :image, {
+                      :styles => { :thumbnail => "100x100#", :medium => '250x250#' },
+                    }.merge(PAPERCLIP_STORAGE_OPTIONS)
 
 
-   # prints / purchasing relation
-   has_many :prints, :autosave => true do
-     def canvas() where(:material => 'canvas') end
-     def photopapers() where(:material => 'photopaper') end
-     def original() where(:material => 'original') end
-   end
+  # prints / purchasing relation
+  has_many :prints, :autosave => true do
+    def canvas() where(:material => 'canvas') end
+    def photopapers() where(:material => 'photopaper') end
+    def original() where(:material => 'original') end
+  end
 
-   scope :for_year, lambda { |year| where("created_at >= ? and created_at < ?", "#{year}-01-01", "#{(year.to_i + 1)}-01-01") }
-   scope :featured, -> { where(featured: true) }
-   scope :fanart, -> { where(fanart: true) }
-   scope :original, -> { where(fanart: false) }
+  scope :for_year, lambda { |year| where("created_at >= ? and created_at < ?", "#{year}-01-01", "#{(year.to_i + 1)}-01-01") }
+  scope :featured, -> { where(featured: true) }
+  scope :fanart, -> { where(fanart: true) }
+  scope :original, -> { where(fanart: false) }
 
-   def self.newest
-     Artwork.limit(1).order('created_at DESC').first
-   end
+  def self.newest
+    Artwork.limit(1).order('created_at DESC').first
+  end
 
-   accepts_nested_attributes_for :prints
+  accepts_nested_attributes_for :prints
 
-   validate :created_at do
-     return true if @created.nil?
+  validate :created_at, :created_at_validator
 
-     # created = 'some date' has been called
-     begin
-         Date.strptime(@created, '%m/%d/%Y')
-     rescue
-         return self.errors.add(:created_at, "Date '#{@created}' is not in the valid format 'mm/dd/yyyy'")
-     end
+  # pretty url stuff
+  extend FriendlyId
+  friendly_id :title, :use => [:slugged, :finders, :history]
 
-     if created_at.year < 1000
-         return self.errors.add(:created_at, "Date should have years formated as yyyy")
-     end
-   end
+  def sizes
+    s = {}
+    prints.each {|print| s[print.dimensions] = true if print.material != "original"}
+    logger.debug "sizes = #{s.keys.sort}"
 
-   # pretty url stuff
-   extend FriendlyId
-   friendly_id :title, :use => [:slugged, :finders, :history]
+    s.keys.sort_by do |a|
+      width, height = a.split "x"
+      [ Integer(width), Integer(height) ]
+    end
+  end
 
-   def sizes
-     s = {}
-     prints.each {|print| s[print.dimensions] = true if print.material != "original"}
-     logger.debug "sizes = #{s.keys.sort}"
+  def created=(string_date)
+    # force invoking the function with nil to store an empty string
+    @created = string_date.to_s
+    return unless string_date
 
-     s.keys.sort_by do |a|
-       width, height = a.split "x"
-       [ Integer(width), Integer(height) ]
-     end
-   end
+    begin
+      self.created_at = Date.strptime(string_date, '%m/%d/%Y')
+    rescue ArgumentError
+      # could not parse in given format
+      # handle in validation of @created
+    end
+  end
 
-   def created=(string_date)
-     # force invoking the function with nil to store an empty string
-     @created = string_date.to_s
-     return unless string_date
+  def price_of(material, size)
+    warn "price_of is deprecated. use find_print.price. called from #{caller.first}"
+    logger.debug "material = #{material}; size = #{size}"
+    print = prints.select {|print| print.material == material.to_s and print.dimensions == size.to_s }.first
+    print.price if print
 
-     begin
-       self.created_at = Date.strptime(string_date, '%m/%d/%Y')
-     rescue ArgumentError
-       # could not parse in given format
-       # handle in validation of @created
-     end
-   end
+    Artwork.price_of material, size
+  end
 
-   def price_of(material, size)
-     warn "price_of is deprecated. use find_print.price. called from #{caller.first}"
-     logger.debug "material = #{material}; size = #{size}"
-     print = prints.select {|print| print.material == material.to_s and print.dimensions == size.to_s }.first
-     print.price if print
+  def find_print(params)
+    prints.where(params).first
+  end
 
-     Artwork.price_of material, size
-   end
+  def self.price_of(material, size)
+    price_point = DefaultPrices.all.select {|price_point| price_point.material == material.to_s and price_point.dimension == size.to_s }.first
+    price_point.price
+  end
 
-   def find_print(params)
-     prints.where(params).first
-   end
+  def has_size?( size_name )
+    prints.where(:size_name => size_name).size > 0 if id
+  end
 
-   def self.price_of(material, size)
-     price_point = DefaultPrices.all.select {|price_point| price_point.material == material.to_s and price_point.dimension == size.to_s }.first
-     price_point.price
-   end
+  def original
+    prints.original.first
+  end
 
-   def has_size?( size_name )
-     prints.where(:size_name => size_name).size > 0 if id
-   end
+  def original?
+    not fanart?
+  end
 
-   def original
-     prints.original.first
-   end
+  delegate :dimensions, :to => :original, :prefix => true
 
-   def original?
-     not fanart?
-   end
+  private
 
-   delegate :dimensions, :to => :original, :prefix => true
+  def created_at_validator
+    return true if @created.nil?
+
+    # created = 'some date' has been called
+    begin
+      Date.strptime(@created, '%m/%d/%Y')
+    rescue
+      return self.errors.add(:created_at, "Date '#{@created}' is not in the valid format 'mm/dd/yyyy'")
+    end
+
+    if created_at.year < 1000
+      return self.errors.add(:created_at, "Date should have years formated as yyyy")
+    end
+
+  end
 end
